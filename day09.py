73b19c8082367ecb1139abca8339c337614b0a81
Ned Batchelder <ned@nedbatchelder.com>
2016-12-10 15:25:18 -0500

    Day 9, puzzle 2. I shouldn't have copied so much code...

--- day09.py
+++ day09.py
@@ -44,5 +44,46 @@ def puzzle1():
         decompressed = decompress(finput.read())
     print(f"Puzzle 1: decompressed length is {len(decompressed)}")
 
+def decompress2(s):
+    while "(" in s:
+        print(f"Length is {len(s):,d}")
+        s = decompress(s)
+    return s
+
+def decompress2_length(s):
+    total = 0
+    i = 0
+    while i < len(s):
+        c = s[i]
+        if c == '(':
+            m = AXB.match(s, i)
+            assert m
+            i += m.end() - m.start()
+            length = int(m.group(1))
+            repeat = int(m.group(2))
+            chunk = s[i:i+length]
+            i += length
+            total += decompress2_length(chunk) * repeat
+        else:
+            if not c.isspace():
+                total += 1
+            i += 1
+    return total
+
+@pytest.mark.parametrize("s, res", [
+    ("(3x3)XYZ", 9),
+    ("X(8x2)(3x3)ABCY", 20),
+    ("(27x12)(20x12)(13x14)(7x10)(1x12)A", 241920),
+    ("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN", 445),
+])
+def test_decompress2_length(s, res):
+    assert decompress2_length(s) == res
+
+def puzzle2():
+    with open("day09_input.txt") as finput:
+        decompressed_length = decompress2_length(finput.read())
+    print(f"Puzzle 2: decompressed length is {decompressed_length}")
+
 if __name__ == "__main__":
     puzzle1()
+    puzzle2()
