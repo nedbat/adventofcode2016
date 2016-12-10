a68b301e44c32c4d8b39231f123902b77e27e2d3
Ned Batchelder <ned@nedbatchelder.com>
2016-12-09 20:19:36 -0500

    Convert to a class

--- day04.py
+++ day04.py
@@ -7,16 +7,20 @@ import re
 
 import pytest
 
-Room = collections.namedtuple("Room", "name, sector_id, checksum")
+class Room(collections.namedtuple("Room", "name, sector_id, checksum")):
 
+    @classmethod
+    def parse(cls, s):
+        match = re.search(r"^([a-z-]+)-(\d+)\[(\w+)\]\s*$", s)
+        name, sector_id, checksum = match.groups()
+        return cls(name, int(sector_id), checksum)
 
-def parse_room(s):
-    match = re.search(r"^([a-z-]+)-(\d+)\[(\w+)\]\s*$", s)
-    name, sector_id, checksum = match.groups()
-    return Room(name, int(sector_id), checksum)
+    def is_real(self):
+        return most_common_5(self.name) == self.checksum
+
 
 def test_parse_room():
-    assert parse_room('aaaaa-bbb-z-y-x-123[abxyz]') == Room("aaaaa-bbb-z-y-x", 123, "abxyz")
+    assert Room.parse('aaaaa-bbb-z-y-x-123[abxyz]') == Room("aaaaa-bbb-z-y-x", 123, "abxyz")
 
 def most_common_5(s):
     c = collections.Counter(s.replace("-", ""))
@@ -34,9 +38,6 @@ def most_common_5(s):
 def test_most_common_5(name, five):
     assert most_common_5(name) == five
 
-def is_real_room(room):
-    return most_common_5(room.name) == room.checksum
-
 @pytest.mark.parametrize("room,real", [
     ("aaaaa-bbb-z-y-x-123[abxyz]", True),
     ("a-b-c-d-e-f-g-h-987[abcde]", True),
@@ -44,10 +45,10 @@ def is_real_room(room):
     ("totally-real-room-200[decoy]", False),
 ])
 def test_is_real_room(room, real):
-    assert is_real_room(parse_room(room)) == real
+    assert Room.parse(room).is_real == real
 
 with open("day04_input.txt") as finput:
-    rooms = list(map(parse_room, finput))
+    rooms = list(map(Room.parse, finput))
 
-total = sum(room.sector_id for room in rooms if is_real_room(room))
+total = sum(room.sector_id for room in rooms if room.is_real())
 print(f"Puzzle 1: the total is {total}")
