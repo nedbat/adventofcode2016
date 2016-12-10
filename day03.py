4cbc95ba6a500b5fc7b5e22ee9ce78d09965d529
Ned Batchelder <ned@nedbatchelder.com>
2016-12-09 19:01:53 -0500

    Day 3, puzzle 2

--- day03.py
+++ day03.py
@@ -24,4 +24,35 @@ with open("day03_input.txt") as inputf:
     nums = [tuple(map(int, line.strip().split())) for line in inputf]
 
 num_valid = sum(valid_triangle(*abc) for abc in nums)
-print(f"There are {num_valid} valid triangles")
+print(f"Puzzle 1: There are {num_valid} valid triangles")
+
+def read_vertically(inputf):
+    inputf = iter(inputf)
+    while True:
+        nums = []
+        for _ in range(3):
+            line = next(inputf)
+            nums.append(tuple(map(int, line.strip().split())))
+        for c in range(3):
+            yield nums[0][c], nums[1][c], nums[2][c]
+
+def test_read_vertically():
+    the_input = [
+        "101 301 501",
+        "102 302 502",
+        "103 303 503",
+        "201 401 601",
+        "202 402 602",
+        "203 403 603",
+    ]
+    nums = list(read_vertically(the_input))
+    assert nums == [
+        (101, 102, 103), (301, 302, 303), (501, 502, 503),
+        (201, 202, 203), (401, 402, 403), (601, 602, 603),
+    ]
+
+with open("day03_input.txt") as inputf:
+    nums = list(read_vertically(inputf))
+
+num_valid = sum(valid_triangle(*abc) for abc in nums)
+print(f"Puzzle 2: There are {num_valid} valid triangles when read vertically")
