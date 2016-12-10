1de68d09ec61ac203c44749979fc61abd8f88ad8
Ned Batchelder <ned@nedbatchelder.com>
2016-12-09 20:51:29 -0500

    Day 4, puzzle 2

--- day04.py
+++ day04.py
@@ -18,6 +18,27 @@ class Room(collections.namedtuple("Room", "name, sector_id, checksum")):
     def is_real(self):
         return most_common_5(self.name) == self.checksum
 
+    def decrypt_name(self):
+        d = []
+        for c in self.name:
+            if c == '-':
+                c = ' '
+            else:
+                c = rotate_letter(c, self.sector_id)
+            d.append(c)
+        return "".join(d)
+
+def rotate_letter(c, num):
+    """Shift the letter c forward by num, rotating to stay in the alphabet."""
+    return chr(((ord(c) - 97) + num) % 26 + 97)
+
+@pytest.mark.parametrize("let,num,res", [
+    ("a", 1, "b"),
+    ("z", 1, "a"),
+    ("a", 27, "b"),
+])
+def test_rotate_letter(let, num, res):
+    assert rotate_letter(let, num) == res
 
 def test_parse_room():
     assert Room.parse('aaaaa-bbb-z-y-x-123[abxyz]') == Room("aaaaa-bbb-z-y-x", 123, "abxyz")
@@ -45,10 +66,18 @@ def test_most_common_5(name, five):
     ("totally-real-room-200[decoy]", False),
 ])
 def test_is_real_room(room, real):
-    assert Room.parse(room).is_real == real
+    assert Room.parse(room).is_real() == real
 
 with open("day04_input.txt") as finput:
     rooms = list(map(Room.parse, finput))
 
 total = sum(room.sector_id for room in rooms if room.is_real())
 print(f"Puzzle 1: the total is {total}")
+
+
+def test_decrypt_name():
+    assert Room.parse("qzmt-zixmtkozy-ivhz-343[xyzzy]").decrypt_name() == "very encrypted name"
+
+for room in rooms:
+    if room.is_real():
+        print(f"Puzzle 2: {room.decrypt_name()} {room.sector_id}")
