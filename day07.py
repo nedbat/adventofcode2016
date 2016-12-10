990c50bfcddc258f53086e497db8f94fa436d8e8
Ned Batchelder <ned@nedbatchelder.com>
2016-12-10 08:46:18 -0500

    Day 7, puzzle 2

--- day07.py
+++ day07.py
@@ -24,9 +24,13 @@ def has_abba(s):
 def test_has_abba(s, abba):
     assert has_abba(s) == abba
 
-def supports_tls(ip):
+def inside_outside(ip):
     inside_brackets = "".join(re.findall(r"\[.*?\]", ip))
     outside_brackets = re.sub(r"\[.*?\]", "#", ip)
+    return inside_brackets, outside_brackets
+
+def supports_tls(ip):
+    inside_brackets, outside_brackets = inside_outside(ip)
     return has_abba(outside_brackets) and not has_abba(inside_brackets)
 
 @pytest.mark.parametrize("ip, supports", [
@@ -38,11 +42,53 @@ def supports_tls(ip):
 def test_supports_tls(ip, supports):
     assert supports_tls(ip) == supports
 
+def all_aba(s):
+    """Return a set of AB strings for the ABA matches."""
+    return {(m.group(2) + m.group(3)) for m in re.finditer(r"(?=((.)(.)\2))", s)}
+
+def all_bab(s):
+    return {aba[1]+aba[0] for aba in all_aba(s)}
+
+@pytest.mark.parametrize("s, abas", [
+    ("zazbz", {"za", "zb"}),
+    ("azawpddzkqbosmltyxt[zoaaqnowmmwkmfkq]lgusvzwnimvgagupkt[scbjhqdftzssbvnvff]coiaslgcrwvyioxx[jouvwdiwvbsembzf]popmlnhjkoaeahcny", {'ae', 'az', 'ga', 'po', 'vn'}),
+])
+def test_all_aba(s, abas):
+    assert all_aba(s) == abas
+
+@pytest.mark.parametrize("s, babs", [
+    ("zazbz", {"az", "bz"}),
+])
+def test_all_bab(s, babs):
+    assert all_bab(s) == babs
+
+def supports_ssl(ip):
+    inside_brackets, outside_brackets = inside_outside(ip)
+    abas = all_aba(outside_brackets)
+    babs = all_bab(inside_brackets)
+    return bool(abas & babs)
+
+@pytest.mark.parametrize("ip, supports", [
+    ("aba[bab]xyz", True),
+    ("xyx[xyx]xyx", False),
+    ("aaa[kek]eke", True),
+    ("zazbz[bzb]cdb", True),
+])
+def test_supports_ssl(ip, supports):
+    assert supports_ssl(ip) == supports
+
 def puzzle1():
     with open("day07_input.txt") as inputf:
-        num_abba = sum(supports_tls(line) for line in inputf)
+        num_tls = sum(supports_tls(line) for line in inputf)
 
-    print(f"Puzzle 1: {num_abba} IPs support TLS")
+    print(f"Puzzle 1: {num_tls} IPs support TLS")
+
+def puzzle2():
+    with open("day07_input.txt") as inputf:
+        num_ssl = sum(supports_ssl(line) for line in inputf)
+
+    print(f"Puzzle 2: {num_ssl} IPs support SSL")
 
 if __name__ == "__main__":
     puzzle1()
+    puzzle2()
