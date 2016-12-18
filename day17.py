431d0b24b7a69cfcedd469ea7056c25e28aa3041
Ned Batchelder <ned@nedbatchelder.com>
2016-12-18 08:43:30 -0500

    Day 17, puzzle 2

--- day17.py
+++ day17.py
@@ -41,10 +41,11 @@ def next_steps(x, y, passcode, path):
 def test_next_steps(x, y, passcode, path, result):
     assert list(next_steps(x, y, passcode, path)) == result
 
-def walk_room(passcode):
+def walk_room(passcode, shortest=True):
     possibilities = [State(1, 1, "")]
+    solution = ""
 
-    while True:
+    while possibilities:
         next_possibilities = []
         for x, y, path in possibilities:
             for direction in next_steps(x, y, passcode, path):
@@ -52,11 +53,16 @@ def walk_room(passcode):
                 new_x = x + DELTAS[direction][0]
                 new_y = y + DELTAS[direction][1]
                 if (new_x, new_y) == (4, 4):
-                    return new_path
+                    if shortest:
+                        return new_path
+                    elif len(new_path) > len(solution):
+                        solution = new_path
                 else:
                     next_possibilities.append(State(new_x, new_y, new_path))
 
         possibilities = next_possibilities
+
+    return solution
 
 @pytest.mark.parametrize("passcode, result", [
     ('ihgpwlah', 'DDRRRD'),
@@ -72,5 +78,19 @@ INPUT = "njfxhljp"
 def puzzle1():
     print(f"Puzzle 1: shortest path is {walk_room(INPUT)}")
 
+
+@pytest.mark.parametrize("passcode, result", [
+    ('ihgpwlah', 370),
+    ('kglvqrro', 492),
+    ('ulqzkmiv', 830),
+])
+def test_walk_room_longest(passcode, result):
+    assert len(walk_room(passcode, shortest=False)) == result
+
+
+def puzzle2():
+    print(f"Puzzle 2: longest path is {len(walk_room(INPUT, shortest=False))} long")
+
 if __name__ == "__main__":
     puzzle1()
+    puzzle2()
